0b77207c1e8b9f8dcb9317911ad11336a32aeac6
Kimoto <kimotohisao17@gmail.com>
2025-01-08 23:38:24 +0900

    first commit

---
+++ src/components/TrendBar/Trendbar.js
@@ -0,0 +1,16 @@
+import './Trendbar.css';
+// import coinImgExample from '../../assets/img/coin_example.png';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export const Trendbar = () => {
+  return (
+    <div className="trendBar">
+      <div className="trendLogo">
+        <span className="trendBarSpan">Trend Bar</span>
+      </div>
+    </div>
+  );
+};
+
+export default Trendbar;
